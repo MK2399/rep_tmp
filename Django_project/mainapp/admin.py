76bd4667282a78b4de65b26de6f03e5bc29c5ff4
Mikita Kuchko <kuchkomikita@gmail.com>
2023-01-15 17:43:03 +0300

    Second commit

--- Django_project/mainapp/admin.py
+++ Django_project/mainapp/admin.py
@@ -1,10 +1,68 @@
 from django.contrib import admin
-from .models import Category, Product
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from django.shortcuts import render, get_object_or_404
+from django.utils.html import format_html
+# from .forms import ProductFormAdmin
+from .models import Category, Product, Company
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name']
+    list_display_links = ['name']
+    search_fields = ['name']
+    ordering = ['name']
+    list_per_page = 5
+    list_filter = ['name']
+    prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'image', 'price', 'available', 'created']
+    list_display = [
+        'name',
+        'price',
+        'available',
+        'category',
+        # 'view_product_link',
+        'ava',
+    ]
+    list_display_links = ['name', 'price']
+    list_editable = ['category', 'available']
+    list_filter = ['category', 'available']
+    prepopulated_fields = {'slug': ('name',) }
+    list_per_page = 5
+    search_fields = ['name', 'price']
+    ordering = ['price']
+    # form = ProductFormAdmin
+
+    @staticmethod
+    def ava(obj):
+        return mark_safe(f"<img class='img-circle' src='{obj.image.url}'"
+                         f"height='30' widht='30'>"
+        )
+
+    # @staticmethod
+    # def view_product_link(request, obj):
+    #     product = get_object_or_404(obj, slug=obj.slug)
+    #     context = {
+    #         'product': product,
+    #     }
+    #     return render(request, 'Product.html', context)
+
+
+    # @staticmethod
+    # def custom_field(obj):
+    #     return mark_safe(f'<a href="{obj.get_absolute_url}/{obj.slug}">кнопка</a>')
+
+    # @staticmethod
+    # def view_on_site(self, obj):
+    #     return reverse('product_edit', kwargs={'pk': obj.pk})
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_display_links = ['name']
+    search_fields = ['name']
+    ordering = ['name']
+    list_per_page = 5
+    list_filter = ['name']
